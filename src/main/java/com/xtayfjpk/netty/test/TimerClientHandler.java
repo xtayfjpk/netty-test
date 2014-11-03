747492cf177f78483a0531d00e9fd593b253ac47
zhangjin <zhangjin@kingyea.com.cn>
2014-11-03 15:36:02 +0800

    第一种解决服务端传递的数据可能被分成碎片的问题方案

--- src/main/java/com/xtayfjpk/netty/test/TimerClientHandler.java
+++ src/main/java/com/xtayfjpk/netty/test/TimerClientHandler.java
@@ -6,18 +6,36 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
+/**
+ * 第一种解决服务端传递的数据可能被分成碎片的问题方案
+ * @author zj
+ *
+ */
 public class TimerClientHandler extends ChannelHandlerAdapter {
+	private ByteBuf buf;
+	
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		this.buf = ctx.alloc().buffer(4);
+	}
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		this.buf.release();
+		this.buf = null;
+	}
 	
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		 ByteBuf m = (ByteBuf) msg; // (1)
-        try {
-            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+		ByteBuf m = (ByteBuf) msg; // (1)
+		buf.writeBytes(m);
+		m.release();
+		 
+		if(buf.readableBytes()>=4) {
+            long currentTimeMillis = (buf.readUnsignedInt() - 2208988800L) * 1000L;
             System.out.println(new Date(currentTimeMillis));
             ctx.close();
-        } finally {
-            m.release();
-        }
+		}
+        
 	}
 	
 	@Override
