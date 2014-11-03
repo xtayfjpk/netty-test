85654e74d80a5b4edee303071354cf3fcfdbb59a
zhangjin <zhangjin@kingyea.com.cn>
2014-11-03 15:23:35 +0800

    添加服务与客户端

---
+++ src/main/java/com/xtayfjpk/netty/test/TimeServer.java
@@ -0,0 +1,52 @@
+package com.xtayfjpk.netty.test;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+	private int port;
+	
+	public TimeServer(int port) {
+		this.port = port;
+	}
+	
+	public void run() throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+				.channel(NioServerSocketChannel.class)
+				.childHandler(new ChannelInitializer<SocketChannel>() {
+					@Override
+					protected void initChannel(SocketChannel ch) throws Exception {
+						ch.pipeline().addLast(new TimerServerHandler());	
+					}
+				})
+				.option(ChannelOption.SO_BACKLOG, 128)
+				.childOption(ChannelOption.SO_KEEPALIVE, true);
+			
+			ChannelFuture future = b.bind(port).sync();
+			
+			future.channel().closeFuture().sync();
+		} finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		int port = 8080;
+		System.out.println("server is ready to run.");
+		new TimeServer(port).run();
+	}
+
+}
