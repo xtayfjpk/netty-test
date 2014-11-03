0f599e6b4ef11cb3156081cd580d62c310740195
zhangjin <zhangjin@kingyea.com.cn>
2014-11-03 15:23:18 +0800

    添加TimerClientHandler

---
+++ src/main/java/com/xtayfjpk/netty/test/TimerClientHandler.java
@@ -0,0 +1,28 @@
+package com.xtayfjpk.netty.test;
+
+import java.util.Date;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class TimerClientHandler extends ChannelHandlerAdapter {
+	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		 ByteBuf m = (ByteBuf) msg; // (1)
+        try {
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+        ctx.close();
+	}
+}
